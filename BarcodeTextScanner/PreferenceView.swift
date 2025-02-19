08a20df473c0cee73c0b91db23b3db5c360b5edf
Riken Shah <rikenshah.02@gmail.com>
2025-02-19 18:12:45 +0530

    fix: ui for user preference

--- BarcodeTextScanner/PreferenceView.swift
+++ BarcodeTextScanner/PreferenceView.swift
@@ -26,31 +26,6 @@ enum Preference: String, CaseIterable {
         }
     }
     
-    var icon: String {
-        switch self {
-        case .vegan:
-            return "🌱"
-        case .vegetarian:
-            return "🥗"
-        case .pescatorian:
-            return "🐟"
-        case .eggetarian:
-            return "🥚"
-        }
-    }
-    
-    var description: String {
-        switch self {
-        case .vegan:
-            return "No animal products"
-        case .vegetarian:
-            return "No meat or fish"
-        case .pescatorian:
-            return "No meat but fish allowed"
-        case .eggetarian:
-            return "No meat or fish but eggs allowed"
-        }
-    }
     
     var blacklistedIngredientGroups: [String] {
         switch self {
@@ -66,87 +41,90 @@ enum Preference: String, CaseIterable {
     }
     }
     
-    struct PreferenceView: View {
-        
-        @State private var selectedPreference: Preference? = nil
-        @AppStorage("preference") private var preference: String?
-        
-        var body: some View {
-            ZStack {
-                SafeBackgroundView()
-                    .ignoresSafeArea()
+struct PreferenceView: View {
+    
+    @State private var selectedPreference: Preference? = nil
+    @AppStorage("preference") private var preference: String?
+    
+    var body: some View {
+        ZStack {
+            SafeBackgroundView()
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Choose Your Dietary Preference")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(.vertical, 20)
                 
-                VStack {
-                    Text("Choose Your Dietary Preference")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .padding(.vertical, 20)
-                    
-                    ForEach(Preference.allCases, id: \.self) { preference in
-                        Button {
-                            withAnimation {
-                                selectedPreference = preference
-                            }
-                        } label: {
-                            VStack(alignment: .leading, spacing: 4) {
-                                HStack {
-                                    Text(preference.icon)
-                                        .font(.title2)
-                                    
-                                    Text(preference.title)
-                                        .font(.headline)
-                                        .foregroundStyle(preference == selectedPreference ? .safeGreen : .white)
-                                    
-                                    Spacer()
-                                    
+                ForEach(Preference.allCases, id: \.self) { preference in
+                    Button {
+                        withAnimation {
+                            selectedPreference = preference
+                        }
+                    } label: {
+                        VStack(alignment: .leading, spacing: 4) {
+                            HStack {
+                                Spacer()
+                                Text(preference.title)
+                                    .font(.headline)
+                                    .foregroundStyle(preference == selectedPreference ? .safeGreen : .white)
+                                Spacer()
+                                
+                                // Reserve space with the same width
+                                Group {
                                     if preference == selectedPreference {
                                         Image(systemName: "checkmark")
                                             .font(.headline)
                                             .foregroundColor(Color.safeGreen)
+                                    } else {
+                                        Image(systemName: "checkmark")
+                                            .font(.headline)
+                                            .foregroundColor(.clear)
                                     }
                                 }
-                                
-                                Text(preference.description)
-                                    .font(.subheadline)
-                                    .foregroundStyle(preference == selectedPreference ? .safeGreen.opacity(0.8) : .white.opacity(0.8))
+                                .frame(width: 20)
                             }
-                            .padding(.vertical, 12)
-                            .padding(.horizontal)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .background {
-                                if preference == selectedPreference {
-                                    Color.white
-                                } else {
-                                    Color.black.opacity(0.1)
-                                }
+                            
+                        }
+                        .padding(.vertical, 20)
+                        .padding(.horizontal, 20)
+                        .frame(maxWidth: .infinity)
+                        .background {
+                            if preference == selectedPreference {
+                                Color.white
+                            } else {
+                                Color.black.opacity(0.1)
                             }
-                            .clipShape(RoundedRectangle(cornerRadius: 12))
                         }
-                        .padding(.bottom, 8)
+                        .clipShape(RoundedRectangle(cornerRadius: 24))
                     }
-                    
-                    Spacer()
-                    
-                    Button {
-                        withAnimation {
-                            preference = selectedPreference?.rawValue
-                        }
-                    } label: {
-                        Text("Get Started")
-                            .font(.headline)
-                            .foregroundStyle(.black)
-                            .padding(.horizontal)
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 50)
-                            .background(Color.white)
-                            .clipShape(Capsule())
-                    }
-                    .disabled(selectedPreference == nil)
+                    .padding(.bottom, 8)
                 }
-                .padding()
+                
+                Spacer()
+                
+                Button {
+                    withAnimation {
+                        preference = selectedPreference?.rawValue
+                    }
+                } label: {
+                    Text("Get Started")
+                        .font(.headline)
+                        .foregroundStyle(.black)
+                        .padding(.horizontal)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 50)
+                        .background(Color.white)
+                        .clipShape(Capsule())
+                }
+                .disabled(selectedPreference == nil)
             }
-            .toolbarVisibility(.hidden)
-     
+            .padding()
         }
+        .toolbarVisibility(.hidden)
+    }
+
+    }
     
